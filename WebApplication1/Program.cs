ad5010fe0d7fd0f9ebbee9d7049e7414d1125621
dipendra-jaiswal <dipendra.jaiswal@hotmail.com>
2025-10-13 19:42:42 +0530

    test2

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,9 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Get logger from builder
-builder.Services.AddLogging();
 // Add services to the container.
-//builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
@@ -15,58 +13,16 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-//Binding default static pages
-//DefaultFilesOptions obj = new DefaultFilesOptions();
-//obj.DefaultFileNames.Clear();
-//obj.DefaultFileNames.Add("MyPage.html");
-
-
-FileServerOptions objFileServer = new FileServerOptions();
-objFileServer.DefaultFilesOptions.DefaultFileNames.Clear();
-objFileServer.DefaultFilesOptions.DefaultFileNames.Add("MyPage.html");
-
-
-
-//app.UseDefaultFiles(obj);
-//app.UseStaticFiles();
-
-//app.UseFileServer(objFileServer);
-
-
 app.UseHttpsRedirection();
 app.UseRouting();
 
-//app.UseAuthorization();
+app.UseAuthorization();
 
 app.MapStaticAssets();
 
-app.Use(async (context, next) =>
-{
-    var logger = app.Services.GetRequiredService<ILogger<Program>>();
-    logger.LogInformation("a1 start");
-    await context.Response.WriteAsync("\ncustom display!");
-    await next();
-    logger.LogInformation("a1 end");
-});
-
-app.Use(async (context, next) =>
-{
-    var logger = app.Services.GetRequiredService<ILogger<Program>>();
-    logger.LogInformation("a2 start");
-    await context.Response.WriteAsync("\ncustom display-2!");
-    await next();
-    logger.LogInformation("a2 end");
-});
-
-
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller=Home}/{action=Index}/{id?}")
-//    .WithStaticAssets();
-
-
-app.Run(async (context) => {
-    await context.Response.WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
-});
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
 
 app.Run();
